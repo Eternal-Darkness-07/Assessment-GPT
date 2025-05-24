647f37b2c6884f5bd3232bc7dd79ad262f55eb2a
Ravi Patel <varshakotadiya1980@gmail.com>
2025-05-25 02:26:36 +0530

    .

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = 'https://assessment-gpt.onrender.com/api';
 
 export const getQuestions = (topic, count) => axios.post(`${API_BASE_URL}/questions/get-questions`, { topic, count }).then(res => res.data.questions);
